b0e56b21605efe5289650870341b44482c4c884a
Tiku <indranilchutia0308@gmail.com>
2024-02-22 03:26:06 +0530

    minor ui tweaks

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -5,13 +5,13 @@ import heroImg from "@images/hero.png"
 const LandingPage = () => {
     return (
         <div className="flex flex-col items-center">
-            <div className="container lg:px-8">
-                <div className="h-screen w-screen flex relative">
-                    <div className="flex flex-col justify-center ">
-                        <h1 className="text-4xl md:text-8xl md:leading-tight">
+            <div className="container px-4 lg:px-8">
+                <div className="h-screen md:w-screen flex relative">
+                    <div className="w-full flex flex-col md:justify-center ">
+                        <h1 className="text-5xl leading-snug font-semibold md:font-normal md:text-8xl md:leading-tight">
                             Find Perfect<br /> Rental Homes<br /> in No Time
                         </h1>
-                        <p className="max-w-96 leading-normal mt-3">We are the best rental home service that promises to shift you into your new home on the same day.</p>
+                        <p className="md:max-w-96 flex flex-wrap leading-normal mt-3">We are the best rental home service that promises to shift you into your new home on the same day.</p>
                         <span className="mt-10">
 
                             <Button
@@ -22,7 +22,7 @@ const LandingPage = () => {
                             </Button>
                         </span>
                     </div>
-                    <img src={heroImg} alt="hero" className="absolute bottom-0 right-0 h-1/2 md:h-fit md:w-3/5" />
+                    <img src={heroImg} alt="hero" className="hidden md:block absolute bottom-0 right-0 h-1/2 md:h-fit md:w-3/5" />
                 </div>
             </div>
         </div>
